61844ad71bab8431b66e7c3b8a64354e9ef9a7c1
Jago128 <jagobaetxegarai@gmail.com>
2025-05-13 09:11:15 +0200

    e

--- src/windows/VentanaMostrarPersona.java
+++ src/windows/VentanaMostrarPersona.java
@@ -191,7 +191,87 @@ public class VentanaMostrarPersona extends JDialog implements ActionListener {
 					System.out.println("Tipo invalido");
 				}
 
-				// Especialidades, SectorInteres [TBD]
+				modelEspecialidad.addElement(p.getEspecialidad());
+				switch (p.getSectorInteres()) {
+				case AGRICULTURA_GANADERIA:
+					modelSectorInteres.addElement("Agricultura y ganadería");
+					break;
+
+				case BIENESCONSUMO:
+					modelSectorInteres.addElement("Bienes de consumo");
+					break;
+
+				case COMERCIOELECTRONICO:
+					modelSectorInteres.addElement("Comercio electrónico");
+					break;
+
+				case COMERCIO_ESTABLECIMIENTOS:
+					modelSectorInteres.addElement("Comercio y establecimientos");
+					break;
+
+				case CONSTRUCCION:
+					modelSectorInteres.addElement("Construcción");
+					break;
+
+				case DEPORTE_OCIO:
+					modelSectorInteres.addElement("Deporte y ocio");
+					break;
+
+				case ENERGIA_MEDIOAMBIENTE:
+					modelSectorInteres.addElement("Energía y medio ambiente");
+					break;
+
+				case FINANZAS_SEGUROS_BIENESINMUEBLES:
+					modelSectorInteres.addElement("Finanzas, seguros y bienes inmuebles");
+					break;
+
+				case INTERNET:
+					modelSectorInteres.addElement("Internet");
+					break;
+
+				case LOGISTICA_TRANSPORTE:
+					modelSectorInteres.addElement("Logística y transporte");
+					break;
+
+				case MEDIOSCOMUNICACION_MARKETING:
+					modelSectorInteres.addElement("Medios de comunicación y marketing");
+					break;
+
+				case METALURGIA_ELECTRONICA:
+					modelSectorInteres.addElement("Metalurgia y electrónica");
+					break;
+
+				case PRODUCTOSQUIMICOS_MATERIASPRIMAS:
+					modelSectorInteres.addElement("Productos químicos y materias primas");
+					break;
+
+				case SALUD_INDUSTRIAFARMACEUTICA:
+					modelSectorInteres.addElement("Salud e industria farmacéutica");
+					break;
+
+				case SERVICIOS:
+					modelSectorInteres.addElement("Servicios");
+					break;
+
+				case SOCIEDAD:
+					modelSectorInteres.addElement("Sociedad");
+					break;
+
+				case TECNOLOGIA_TELECOMUNICACIONES:
+					modelSectorInteres.addElement("Tecnología y telecomunicaciones");
+					break;
+
+				case TURISMO_HOSTELERIA:
+					modelSectorInteres.addElement("Turismo y hostelería");
+					break;
+
+				case VIDA:
+					modelSectorInteres.addElement("Vida");
+					break;
+
+				default:
+					System.out.println("Tipo incorrecto");
+				}
 
 				switch (p.getCerfificadoDiscapacidad()) {
 				case NO:
@@ -281,7 +361,485 @@ public class VentanaMostrarPersona extends JDialog implements ActionListener {
 				}
 
 				switch (p.getLocalidad()) {
-				// TBD
+				case ABADIÑO:
+					modelLocalidad.addElement("Abadiño");
+					break;
+
+				case ABANTO_ZIERBENA:
+					modelLocalidad.addElement("Abanto-Zierbena");
+					break;
+
+				case AJANGIZ:
+					modelLocalidad.addElement("Ajangiz");
+					break;
+
+				case ALONSOTEGI:
+					modelLocalidad.addElement("Alonsotegi");
+					break;
+
+				case AMOREBIETA:
+					modelLocalidad.addElement("Amorebieta");
+					break;
+
+				case AMOROTO:
+					modelLocalidad.addElement("Amoroto");
+					break;
+
+				case AMURRIO:
+					modelLocalidad.addElement("Amurrio");
+					break;
+
+				case ARAKALDO:
+					modelLocalidad.addElement("Arakaldo");
+					break;
+
+				case ARANTZAZU:
+					modelLocalidad.addElement("Arantzazu");
+					break;
+
+				case AREATZA_BILARO:
+					modelLocalidad.addElement("Areatza o Bilaro");
+					break;
+
+				case ARRANKUDIAGA:
+					modelLocalidad.addElement("Arrankudiaga");
+					break;
+
+				case ARRATZU:
+					modelLocalidad.addElement("Arratzu");
+					break;
+
+				case ARRIETA:
+					modelLocalidad.addElement("Arrieta");
+					break;
+
+				case ARRIGORRIAGA:
+					modelLocalidad.addElement("Arrigorriaga");
+					break;
+
+				case ARTZENTALES:
+					modelLocalidad.addElement("Artzentales");
+					break;
+
+				case ARTZINIEGA:
+					modelLocalidad.addElement("Artziniega");
+					break;
+
+				case AULESTI:
+					modelLocalidad.addElement("Aulesti");
+					break;
+
+				case AXPEATXONDO:
+					modelLocalidad.addElement("Axpe Atxondo");
+					break;
+
+				case AYALA_AIARA:
+					modelLocalidad.addElement("Ayala/Aiara");
+					break;
+
+				case BAKIO:
+					modelLocalidad.addElement("Bakio");
+					break;
+
+				case BALMASEDA:
+					modelLocalidad.addElement("Balmaseda");
+					break;
+
+				case BARAKALDO:
+					modelLocalidad.addElement("Barakaldo");
+					break;
+
+				case BARRIKA:
+					modelLocalidad.addElement("Barrika");
+					break;
+
+				case BASAURI:
+					modelLocalidad.addElement("Basauri");
+					break;
+
+				case BEDIA:
+					modelLocalidad.addElement("Bedia");
+					break;
+
+				case BERANGO:
+					modelLocalidad.addElement("Berango");
+					break;
+
+				case BERMEO:
+					modelLocalidad.addElement("Bermeo");
+					break;
+
+				case BERRIATUA:
+					modelLocalidad.addElement("Berriatua");
+					break;
+
+				case BERRIZ:
+					modelLocalidad.addElement("Berriz");
+					break;
+
+				case BILBAO:
+					modelLocalidad.addElement("Bilbao");
+					break;
+
+				case BUSTURIA:
+					modelLocalidad.addElement("Busturia");
+					break;
+
+				case CASTROURDIALES:
+					modelLocalidad.addElement("Castro Urdiales");
+					break;
+
+				case DERIO:
+					modelLocalidad.addElement("Derio");
+					break;
+
+				case DIMA:
+					modelLocalidad.addElement("Dima");
+					break;
+
+				case DURANGO:
+					modelLocalidad.addElement("Durango");
+					break;
+
+				case EA:
+					modelLocalidad.addElement("Ea");
+					break;
+
+				case ELANTXOBE:
+					modelLocalidad.addElement("Elantxobe");
+					break;
+
+				case ELORRIO:
+					modelLocalidad.addElement("Elorrio");
+					break;
+
+				case ERANDIO:
+					modelLocalidad.addElement("Erandio");
+					break;
+
+				case EREÑO:
+					modelLocalidad.addElement("Ereño");
+					break;
+
+				case ERMUA:
+					modelLocalidad.addElement("Ermua");
+					break;
+
+				case ERRIGOITI:
+					modelLocalidad.addElement("Errigoiti");
+					break;
+
+				case ETXEBARRI:
+					modelLocalidad.addElement("Etxebarri");
+					break;
+
+				case ETXEBARRIA:
+					modelLocalidad.addElement("Etxebarria");
+					break;
+
+				case FORUA:
+					modelLocalidad.addElement("Forua");
+					break;
+
+				case FRUIZ:
+					modelLocalidad.addElement("Fruiz");
+					break;
+
+				case GALDAKAO:
+					modelLocalidad.addElement("Galdakao");
+					break;
+
+				case GALDAMES:
+					modelLocalidad.addElement("Galdames");
+					break;
+
+				case GAMIZFIKA:
+					modelLocalidad.addElement("Gamiz-Fika");
+					break;
+
+				case GARAI:
+					modelLocalidad.addElement("Garai");
+					break;
+
+				case GATIKA:
+					modelLocalidad.addElement("Gatika");
+					break;
+
+				case GAUTEGIZ:
+					modelLocalidad.addElement("Gautegiz");
+					break;
+
+				case GAZTELUELEXABEITIA_ARTEAGA:
+					modelLocalidad.addElement("Gaztelu-Elexabeitia o Arteaga");
+					break;
+
+				case GERNIKALUMO:
+					modelLocalidad.addElement("Gernika-Lumo");
+					break;
+
+				case GETXO:
+					modelLocalidad.addElement("Getxo");
+					break;
+
+				case GIZABURUAGA:
+					modelLocalidad.addElement("Gizaburuaga");
+					break;
+
+				case GORDEXOLA:
+					modelLocalidad.addElement("Gordexola");
+					break;
+
+				case GORLIZ:
+					modelLocalidad.addElement("Gorliz");
+					break;
+
+				case GUEÑES:
+					modelLocalidad.addElement("Gueñes");
+					break;
+
+				case IBARRANGELU:
+					modelLocalidad.addElement("Ibarrangelu");
+					break;
+
+				case IGORRE:
+					modelLocalidad.addElement("Igorre");
+					break;
+
+				case ISPASTER:
+					modelLocalidad.addElement("Ispaster");
+					break;
+
+				case IURRETA:
+					modelLocalidad.addElement("Iurreta");
+					break;
+
+				case IZURTZA:
+					modelLocalidad.addElement("Izurtza");
+					break;
+
+				case KARRANTZAHARANA:
+					modelLocalidad.addElement("Karrantza Harana");
+					break;
+
+				case KORTEZUBI:
+					modelLocalidad.addElement("Kortezubi");
+					break;
+
+				case LANESTOSA:
+					modelLocalidad.addElement("Lanestosa");
+					break;
+
+				case LARRABETZU:
+					modelLocalidad.addElement("Larrabetzu");
+					break;
+
+				case LAUDIO_LLODIO:
+					modelLocalidad.addElement("Laudio/Llodio");
+					break;
+
+				case LAUKIZ:
+					modelLocalidad.addElement("Laukiz");
+					break;
+
+				case LEIOA:
+					modelLocalidad.addElement("Leioa");
+					break;
+
+				case LEKEITIO:
+					modelLocalidad.addElement("Lekeitio");
+					break;
+
+				case LEMOA:
+					modelLocalidad.addElement("Lemoa");
+					break;
+
+				case LEMOIZ:
+					modelLocalidad.addElement("Lemoiz");
+					break;
+
+				case LEZAMA:
+					modelLocalidad.addElement("Lezama");
+					break;
+
+				case LOIU:
+					modelLocalidad.addElement("Loiu");
+					break;
+
+				case MALLABIA:
+					modelLocalidad.addElement("Mallabia");
+					break;
+
+				case MARKINAXEMEIN:
+					modelLocalidad.addElement("Markina-Xemein");
+					break;
+
+				case MARURI:
+					modelLocalidad.addElement("Maruri");
+					break;
+
+				case MAÑARIA:
+					modelLocalidad.addElement("Mañaria");
+					break;
+
+				case MENDATA:
+					modelLocalidad.addElement("Mendata");
+					break;
+
+				case MENDEXA:
+					modelLocalidad.addElement("Mendexa");
+					break;
+
+				case MEÑAKA:
+					modelLocalidad.addElement("Meñaka");
+					break;
+
+				case MORGA:
+					modelLocalidad.addElement("Morga");
+					break;
+
+				case MUNDAKA:
+					modelLocalidad.addElement("Mundaka");
+					break;
+
+				case MUNGIA:
+					modelLocalidad.addElement("Mungia");
+					break;
+
+				case MUNITIBARARBATZEGI_GERRIKAITZ:
+					modelLocalidad.addElement("Munitibar-Arbatzegi Gerrikaitz");
+					break;
+
+				case MURUETA:
+					modelLocalidad.addElement("Murueta");
+					break;
+
+				case MUSKIZ:
+					modelLocalidad.addElement("Muskiz");
+					break;
+
+				case MUXIKA:
+					modelLocalidad.addElement("Muxika");
+					break;
+
+				case NABARNIZ:
+					modelLocalidad.addElement("Nabarniz");
+					break;
+
+				case ONDARROA:
+					modelLocalidad.addElement("Ondarroa");
+					break;
+
+				case ORDUÑA:
+					modelLocalidad.addElement("Orduña");
+					break;
+
+				case OROZKO:
+					modelLocalidad.addElement("Orozko");
+					break;
+
+				case ORTUELLA:
+					modelLocalidad.addElement("Ortuella");
+					break;
+
+				case OTXANDIO:
+					modelLocalidad.addElement("Otxandio");
+					break;
+
+				case PLENTZIA:
+					modelLocalidad.addElement("Plentzia");
+					break;
+
+				case PORTUGALETE:
+					modelLocalidad.addElement("Portugalete");
+					break;
+
+				case SANTURTZI:
+					modelLocalidad.addElement("Santurtzi");
+					break;
+
+				case SESTAO:
+					modelLocalidad.addElement("Sestao");
+					break;
+
+				case SONDIKA:
+					modelLocalidad.addElement("Sondika");
+					break;
+
+				case SOPELA:
+					modelLocalidad.addElement("Sopela");
+					break;
+
+				case SOPUERTA:
+					modelLocalidad.addElement("Sopuerta");
+					break;
+
+				case SUKARRIETA:
+					modelLocalidad.addElement("Sukarrieta");
+					break;
+
+				case TRAPAGARAN:
+					modelLocalidad.addElement("Trapagaran");
+					break;
+
+				case TURTZIOZ:
+					modelLocalidad.addElement("Turtzioz");
+					break;
+
+				case UBIDE:
+					modelLocalidad.addElement("Ubide");
+					break;
+
+				case UGAOMIRABALLES:
+					modelLocalidad.addElement("Ugao-Miraballes");
+					break;
+
+				case URDULIZ:
+					modelLocalidad.addElement("Urduliz");
+					break;
+
+				case URDUÑA:
+					modelLocalidad.addElement("Urduña");
+					break;
+
+				case USANSOLO:
+					modelLocalidad.addElement("Usansolo");
+					break;
+
+				case ZALDIBAR:
+					modelLocalidad.addElement("Zaldibar");
+					break;
+
+				case ZALLA:
+					modelLocalidad.addElement("Zalla");
+					break;
+
+				case ZAMUDIO:
+					modelLocalidad.addElement("Zamudio");
+					break;
+
+				case ZARATAMO:
+					modelLocalidad.addElement("Zaratamo");
+					break;
+
+				case ZEANURI:
+					modelLocalidad.addElement("Zeanuri");
+					break;
+
+				case ZEBERIO:
+					modelLocalidad.addElement("Zeberio");
+					break;
+
+				case ZIERBENA:
+					modelLocalidad.addElement("Zierbena");
+					break;
+
+				case ZIORTZA_BOLIBAR:
+					modelLocalidad.addElement("Ziortza-Bolibar");
+					break;
+
+				case ZORNOTZA:
+					modelLocalidad.addElement("Zornotza");
+					break;
 
 				default:
 					System.out.println("Tipo invalido");
@@ -310,7 +868,7 @@ public class VentanaMostrarPersona extends JDialog implements ActionListener {
 				modelOtrosIdiomas.addElement(p.getOtrosIdiomas());
 				modelObservaciones.addElement(p.getObservaciones());
 			}
-			
+
 			listNom.setModel(modelNom);
 			listApoyo.setModel(modelApoyo);
 			listFormacion.setModel(modelFormacion);
