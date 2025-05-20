dda6da630b2de5d9e85350c4e6db52aef3bc67bd
Jago128 <jagobaetxegarai@gmail.com>
2025-05-20 09:35:01 +0200

    Image test

--- src/windows/VentanaAnalisisPuesto.java
+++ src/windows/VentanaAnalisisPuesto.java
@@ -17,38 +17,45 @@ public class VentanaAnalisisPuesto extends JDialog implements ActionListener {
 
 	public VentanaAnalisisPuesto(JFrame parent, LoginController cont, Usuario user) {
 		super(parent, true);
+		setBackground(new Color(255, 255, 255));
+		getContentPane().setBackground(new Color(255, 255, 255));
 		this.cont = cont;
 		this.user = user;
 
 		setResizable(false);
 		setTitle("Analisis de puestos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 400, 180);
+		setBounds(100, 100, 460, 210);
 		getContentPane().setLayout(null);
 
 		btnAñadir = new JButton("Añadir");
 		btnAñadir.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnAñadir.setBounds(215, 76, 145, 45);
+		btnAñadir.setBounds(191, 115, 145, 45);
 		getContentPane().add(btnAñadir);
 
 		btnBorrar = new JButton("Borrar");
 		btnBorrar.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnBorrar.setBounds(215, 21, 145, 45);
+		btnBorrar.setBounds(20, 115, 145, 45);
 		getContentPane().add(btnBorrar);
 
 		btnMostrar = new JButton("Mostrar");
 		btnMostrar.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnMostrar.setBounds(44, 21, 145, 45);
+		btnMostrar.setBounds(236, 10, 145, 45);
 		getContentPane().add(btnMostrar);
 
 		JTextArea textAreaInfoModificar = new JTextArea();
 		textAreaInfoModificar.setEditable(false);
-		textAreaInfoModificar.setBackground(UIManager.getColor("Button.background"));
+		textAreaInfoModificar.setBackground(new Color(255, 255, 255));
 		textAreaInfoModificar.setLineWrap(true);
 		textAreaInfoModificar.setText("Se modifican los analisis de puestos\r\ndesde esta ventana.");
 		textAreaInfoModificar.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		textAreaInfoModificar.setBounds(10, 68, 195, 34);
+		textAreaInfoModificar.setBounds(236, 54, 195, 34);
 		getContentPane().add(textAreaInfoModificar);
+		
+		JLabel logo = new JLabel("");
+		logo.setIcon(new ImageIcon(VentanaAnalisisPuesto.class.getResource("/img/LOGOAPNABI.png")));
+		logo.setBounds(10, 10, 225, 95);
+		getContentPane().add(logo);
 
 		btnAñadir.addActionListener(this);
 		btnBorrar.addActionListener(this);
